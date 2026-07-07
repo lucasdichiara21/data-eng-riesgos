31789bdde413fd21436ba79c2bf1128a951d8a63
lucas dichiara <lucasdichiara21@gmail.com>
2026-07-07 15:46:16 -0400

    fix: corregir sintaxis CASE en Caso 10

--- sql/reports/caso_10_percentil_retiros.sql
+++ sql/reports/caso_10_percentil_retiros.sql
@@ -1,21 +1,21 @@
-WITH retiros AS(
-    SELECT
-        id_cliente,
-        monto,
-    FROM `riesgos.transacciones`
-    WHERE tipo = 'retiro'
-        AND fecha >= DATE_SUB(CURRENT_DATE(), INTERVAL 90 DAY)
-
+-- Caso 10: Percentil 95 de retiros (VaR) últimos 90 días
+WITH retiros AS (
+  SELECT 
+    id_cliente,
+    monto
+  FROM `riesgos.transacciones`
+  WHERE tipo = 'RETIRO'
+    AND fecha >= DATE_SUB(CURRENT_DATE(), INTERVAL 90 DAY)
 )
 SELECT 
-    id_cliente,
-    APPROX_QUANTILES(monto, 100)[OFFSET(94)] AS percentil_95_retiros
-    CASE 
-        WHEN monto > APPROX_QUANTILES(monto, 100)[OFFSET(94)]>10000
-        THEN TRUE
-        ELSE FALSE
-    END AS riesgo_alto
-    GROUP BY id_cliente
-    ORDER BY percentil_95_retiros DESC
-    LIMIT 100;
-    
+  id_cliente,
+  APPROX_QUANTILES(monto, 100)[OFFSET(94)] AS percentil_95_retiros,
+  CASE 
+    WHEN APPROX_QUANTILES(monto, 100)[OFFSET(94)] > 10000 
+    THEN TRUE 
+    ELSE FALSE 
+  END AS riesgo_alto
+FROM retiros
+GROUP BY id_cliente
+ORDER BY percentil_95_retiros DESC
+LIMIT 100;
