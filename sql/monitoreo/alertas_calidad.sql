3e6491bf979b56a40a63d6fc1d08c5dceb4a072a
lucas dichiara <lucasdichiara21@gmail.com>
2026-07-09 16:56:32 -0400

    correccion de errores sql

--- sql/monitoreo/alertas_calidad.sql
+++ sql/monitoreo/alertas_calidad.sql
@@ -1,7 +1,7 @@
-SELECT 
-  DATE(fecha_intento) AS dia,
-  proceso,
-  COUNT(*) AS total_errores
+SELECT
+    proceso,
+    DATE(fecha_intento) AS dia,
+    COUNT(*) AS total_errores
 FROM `riesgos.dead_letter_transacciones`
 WHERE fecha_intento >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 24 HOUR)
 GROUP BY dia, proceso
